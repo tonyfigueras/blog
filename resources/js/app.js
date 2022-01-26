9b6dc72a78a47f3cec8a5d294b8f4c679c8c7a79
Tony Figueras <tonfy01@gmail.com>
2022-01-25 22:17:42 -0400

    instalacion y configuracion de Vue Router y ajustes de la plantilla adminlte

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,18 +3,28 @@ require('./bootstrap');
 
 window.Vue = require('vue').default;
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+//window.Vue = require('vue');
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+import VueRouter from 'vue-router'
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.use(VueRouter)
+
+let routes = [
+	{ path: '/dashboard', component: require('./components/Dashboard.vue').default },
+	{ path: '/profile', component: require('./components/Profile.vue').default }
+]
+
+const router = new VueRouter({
+	mode: 'history',
+	routes,
+	linkActiveClass: 'active',
+	linkExactActiveClass: "exact-active",
+})
+
+const app = new Vue({
+    el: '#app',
+    router
+});
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
